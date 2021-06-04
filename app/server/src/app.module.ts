3b2bf973b65e20d6486377d8d8adc9f0ed261e71
WebMakaka <marley.nodejs@gmail.com>
2021-06-04 09:51:24 +0300

    Starts without review module

--- app/server/src/app.module.ts
+++ app/server/src/app.module.ts
@@ -2,11 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getMongoConfig } from 'configs/mongo.config';
 import { TypegooseModule } from 'nestjs-typegoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
-import { ReviewModule } from './review/review.module';
 import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
@@ -20,9 +17,7 @@ import { TopPageModule } from './top-page/top-page.module';
     AuthModule,
     TopPageModule,
     ProductModule,
-    ReviewModule,
+    // ReviewModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

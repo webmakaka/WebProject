af2fdbb67ee2cfbb78f9c76ba2c0c28e3ffea247
WebMakaka <marley.nodejs@gmail.com>
2021-06-22 12:56:36 +0300

    07 Простые компоненты | 001 Шрифты и цвета

--- app/client/pages/_app.tsx
+++ app/client/pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 export default MyApp;

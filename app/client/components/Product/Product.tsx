0bcc9b7cd4f9d1a80ad45e0a437f056053ac9805
WebMakaka <marley.nodejs@gmail.com>
2021-07-14 03:48:50 +0300

    17 Анимация на Framer Motion | 007 Упражнение - Анимация отзывов

--- app/client/components/Product/Product.tsx
+++ app/client/components/Product/Product.tsx
@@ -22,6 +22,11 @@ export const Product = motion(
       const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
       const reviewRef = useRef<HTMLDivElement>(null);
 
+      const variants = {
+        visible: { opacity: 1, height: 'auto' },
+        hidden: { opacity: 0, height: 0 },
+      };
+
       const scrollToReview = () => {
         setIsReviewOpened(true);
         reviewRef.current.scrollIntoView({
@@ -111,22 +116,21 @@ export const Product = motion(
               </Button>
             </div>
           </Card>
-          <Card
-            color="blue"
-            className={cn(styles.reviews, {
-              [styles.opened]: isReviewOpened,
-              [styles.closed]: !isReviewOpened,
-            })}
-            ref={reviewRef}
+          <motion.div
+            animate={isReviewOpened ? 'visible' : 'hidden'}
+            variants={variants}
+            initial="hidden"
           >
-            {product.reviews.map((r) => (
-              <div key={r._id}>
-                <Review review={r} />
-                <Divider />
-              </div>
-            ))}
-            <ReviewForm productId={product._id} />
-          </Card>
+            <Card color="blue" className={styles.reviews} ref={reviewRef}>
+              {product.reviews.map((r) => (
+                <div key={r._id}>
+                  <Review review={r} />
+                  <Divider />
+                </div>
+              ))}
+              <ReviewForm productId={product._id} />
+            </Card>
+          </motion.div>
         </div>
       );
     }
